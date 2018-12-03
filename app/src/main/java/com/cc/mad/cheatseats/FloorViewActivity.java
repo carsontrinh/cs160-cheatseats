6b536edf433eee4f1f56412c86181d29f3087f0e
Carson Trinh <carsontrinh@gmail.com>
2018-12-02 16:52:49 -0800

    A ton of changes for the floor view as well as added more conistent use of res values.

--- app/src/main/java/com/cc/mad/cheatseats/FloorViewActivity.java
+++ app/src/main/java/com/cc/mad/cheatseats/FloorViewActivity.java
@@ -3,6 +3,7 @@ package com.cc.mad.cheatseats;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -31,16 +32,25 @@ public class FloorViewActivity extends BaseActivity {
         TextView textView_floorName = findViewById(R.id.floor_name);
         TextView textView_divider = findViewById(R.id.divider);
         TextView textView_hours = findViewById(R.id.hours_text);
-        TextView textView_booking = findViewById(R.id.booking_text);
-        TextView textView_floorplan = findViewById(R.id.floorplan_text);
-        TextView textView_phone = findViewById(R.id.number_text);
 
-        textView_spaceName.setText(spaceCardItem.getSpaceName());
-        textView_spaceType.setText(spaceCardItem.getSpaceType());
-        textView_floorName.setText(floorItem.getName());
-        textView_booking.setText(floorItem.getLinkBooking());
-        textView_floorplan.setText(floorItem.getLinkFloorPlan());
-        textView_phone.setText(floorItem.getPhoneNumber());
+        Button button_bookRooms, button_call, button_floorPlans, button_popularTimes;
+        button_bookRooms = findViewById(R.id.book_rooms_button);
+        button_call = findViewById(R.id.phone_number_button);
+        button_floorPlans = findViewById(R.id.floor_plans_button);
+        button_popularTimes = findViewById(R.id.popular_times_button);
+
+        button_popularTimes.setEnabled(false);
+
+//        TextView textView_booking = findViewById(R.id.booking_text);
+//        TextView textView_floorplan = findViewById(R.id.floorplan_text);
+//        TextView textView_phone = findViewById(R.id.number_text);
+//
+//        textView_spaceName.setText(spaceCardItem.getSpaceName());
+//        textView_spaceType.setText(spaceCardItem.getSpaceType());
+//        textView_floorName.setText(floorItem.getName());
+//        textView_booking.setText(floorItem.getLinkBooking());
+//        textView_floorplan.setText(floorItem.getLinkFloorPlan());
+//        textView_phone.setText(floorItem.getPhoneNumber());
 
         if (spaceCardItem.getFloors().size() == 1) {
             textView_floorName.setVisibility(View.GONE);
