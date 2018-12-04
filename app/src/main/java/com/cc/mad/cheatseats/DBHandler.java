e79746a76f88e51291598a0267d2004c49c62364
Alan Yu <yualan14@gmail.com>
2018-12-03 21:15:58 -0800

    Added in changes to make database work

--- app/src/main/java/com/cc/mad/cheatseats/DBHandler.java
+++ app/src/main/java/com/cc/mad/cheatseats/DBHandler.java
@@ -23,7 +23,7 @@ public class DBHandler extends SQLiteOpenHelper {
         String CREATE_DETAIL_TABLE = "CREATE TABLE " + TABLE_DETAIL + "("
                 + KEY_ID + " TEXT,"
                 + KEY_LIBRARY + " TEXT,"
-                + KEY_RATING + " INT" + ")";
+                + KEY_RATING + " INT " + ")";
 
         db.execSQL(CREATE_DETAIL_TABLE);
     }
@@ -33,22 +33,6 @@ public class DBHandler extends SQLiteOpenHelper {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_DETAIL);
 
         onCreate(db);
-    }
-
-    public String loadHandler() {
-        String result = "";
-        String query = "SELECT * FROM " + TABLE_DETAIL;
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
-        while (cursor.moveToNext()) {
-            String result_0 = cursor.getString(0);
-            int result_1 = cursor.getInt(1);
-            result += String.valueOf(result_0) + " " + result_1 +
-                    System.getProperty("line.separator");
-        }
-        cursor.close();
-        db.close();
-        return result;
     }
 
     public void addHandler(Response response) {
@@ -61,49 +45,27 @@ public class DBHandler extends SQLiteOpenHelper {
         db.close();
     }
 
-    public boolean updateHandler(String userID, String library, int rating) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues args = new ContentValues();
-        args.put(KEY_ID, userID);
-        args.put(KEY_LIBRARY, library);
-        args.put(KEY_RATING, rating);
-        return db.update(TABLE_DETAIL, args, KEY_ID + " = ? AND " + KEY_LIBRARY + " = ?", new String[]{userID, library}) > 0;
-    }
-
-    public Response findHandler(String userID, String library) {
-        String query = "Select * FROM " + TABLE_DETAIL + "WHERE" + KEY_ID + " = " + "'" + userID + "'" + "AND" + KEY_LIBRARY + " = " + "'" + library + "'";
-        SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.rawQuery(query, null);
-        Response response = new Response();
-        if (cursor.moveToFirst()) {
-            cursor.moveToFirst();
-            response.setID(cursor.getString(0));
-            response.setLibraryName(cursor.getString(1));
-            response.setRating(Integer.parseInt(cursor.getString(2)));
-            cursor.close();
-        } else {
-            response = null;
-        }
-        db.close();
-        return response;
-    }
-
-    public long getCount(int rating, String library) {
+    public int getCount(int rating, String libraryID) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor cursor= db.rawQuery(
-                "SELECT COUNT (*) FROM " + TABLE_DETAIL + " WHERE " + KEY_RATING + "= ? AND " + KEY_LIBRARY + "= ?",
-                new String[] { String.valueOf(rating), library}
-        );
+        Cursor cursor = db.rawQuery("SELECT  COUNT(*) FROM " + TABLE_DETAIL + " WHERE " + KEY_RATING +
+                "=? AND " + KEY_LIBRARY + "=?", new String[]{String.valueOf(rating), libraryID});
         int count = 0;
-        if(null != cursor) {
-            if(cursor.getCount() > 0){
+        if (null != cursor) {
+            if (cursor.getCount() > 0) {
                 cursor.moveToFirst();
                 count = cursor.getInt(0);
             }
-        cursor.close();
+            cursor.close();
         }
-
         db.close();
         return count;
     }
+
+    public long getTotalCount() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        long count = DatabaseUtils.queryNumEntries(db, TABLE_DETAIL);
+        db.close();
+        return count;
+    }
+
 }
--- app/src/main/java/com/cc/mad/cheatseats/FloorViewActivity.java
+++ app/src/main/java/com/cc/mad/cheatseats/FloorViewActivity.java
@@ -1,8 +1,10 @@
 package com.cc.mad.cheatseats;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -16,23 +18,23 @@ public class FloorViewActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_view);
+
         Intent intent = getIntent();
         floorItem = (FloorItem) intent.getSerializableExtra("FloorItem");
         spaceCardItem = floorItem.getSpaceCardItem();
 
-        final String libraryID = spaceCardItem.getSpaceName() + floorItem.getName();
-        final String iid = "TEMP";
-
-        rate_low = findViewById(R.id.rate_low);
+        rate_low = (ImageView) findViewById(R.id.rate_low);
         final TextView select_low = findViewById(R.id.select_low);
+        final String userID = "TEMP";
+        final String libraryID = String.valueOf(spaceCardItem.getSpaceName()) + String.valueOf(floorItem.getName());
         rate_low.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 DBHandler dbHandler = new DBHandler(FloorViewActivity.this);
-                Response data = new Response(0, libraryID, iid);
-                dbHandler.addHandler(data);
-                String output = "Rated - Low, count: " + dbHandler.getCount(0, libraryID);
-                select_low.setText(output);
+                Response toAdd = new Response(0, libraryID, userID);
+                dbHandler.addHandler(toAdd);
+                select_low.setText(String.valueOf(dbHandler.getCount(0, libraryID)));
+                dbHandler.close();
             }
         });
         rate_medium = findViewById(R.id.rate_medium);
@@ -40,10 +42,10 @@ public class FloorViewActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 DBHandler dbHandler = new DBHandler(FloorViewActivity.this);
-                Response data = new Response(1, libraryID, iid);
-                dbHandler.addHandler(data);
-                String output = "Rated - Medium, count: " + dbHandler.getCount(1, libraryID);
-                select_low.setText(output);
+                Response toAdd = new Response(1, libraryID, userID);
+                dbHandler.addHandler(toAdd);
+                select_low.setText(String.valueOf(dbHandler.getCount(1, libraryID)));
+                dbHandler.close();
             }
         });
         rate_high = findViewById(R.id.rate_high);
@@ -51,10 +53,10 @@ public class FloorViewActivity extends BaseActivity {
             @Override
             public void onClick(View view) {
                 DBHandler dbHandler = new DBHandler(FloorViewActivity.this);
-                Response data = new Response(2, libraryID, iid);
-                dbHandler.addHandler(data);
-                String output = "Rated - High, count: " + dbHandler.getCount(2, libraryID);
-                select_low.setText(output);
+                Response toAdd = new Response(2, libraryID, userID);
+                dbHandler.addHandler(toAdd);
+                select_low.setText(String.valueOf(dbHandler.getCount(2, libraryID)));
+                dbHandler.close();
             }
         });
 
@@ -67,20 +69,57 @@ public class FloorViewActivity extends BaseActivity {
         TextView textView_floorName = findViewById(R.id.floor_name);
         TextView textView_divider = findViewById(R.id.divider);
         TextView textView_hours = findViewById(R.id.hours_text);
-        TextView textView_booking = findViewById(R.id.booking_text);
-        TextView textView_floorplan = findViewById(R.id.floorplan_text);
-        TextView textView_phone = findViewById(R.id.number_text);
+
+        Button button_bookRooms = findViewById(R.id.book_rooms_button);
+        Button button_call = findViewById(R.id.phone_number_button);
+        Button button_floorPlans = findViewById(R.id.floor_plans_button);
+        Button button_popularTimes = findViewById(R.id.popular_times_button);
 
         textView_spaceName.setText(spaceCardItem.getSpaceName());
         textView_spaceType.setText(spaceCardItem.getSpaceType());
         textView_floorName.setText(floorItem.getName());
-        textView_booking.setText(floorItem.getLinkBooking());
-        textView_floorplan.setText(floorItem.getLinkFloorPlan());
-        textView_phone.setText(floorItem.getPhoneNumber());
+
+        button_popularTimes.setEnabled(false);  // Not yet implemented
+
+        // Disable buttons if the floor does not have that info available.
+        if (floorItem.getLinkBooking().equalsIgnoreCase("")) {
+            button_bookRooms.setEnabled(false);
+        }
+        if (floorItem.getPhoneNumber().equalsIgnoreCase("")) {
+            button_call.setEnabled(false);
+        }
+        if (floorItem.getLinkFloorPlan().equalsIgnoreCase("")) {
+            button_floorPlans.setEnabled(false);
+        }
+
+        // TODO update hours using the current day
 
         if (spaceCardItem.getFloors().size() == 1) {
             textView_floorName.setVisibility(View.GONE);
             textView_divider.setVisibility(View.GONE);
         }
     }
+
+    public void phoneOnClick(View view) {
+        String phoneNumber = floorItem.getPhoneNumber();
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL, Uri.fromParts(
+                "tel", phoneNumber, null));
+        startActivity(phoneIntent);
+    }
+
+    public void bookRoomsOnClick(View view) {
+        openWebURI(Uri.parse(floorItem.getLinkBooking()));
+    }
+
+    public void floorPlansOnClick(View view) {
+        openWebURI(Uri.parse(floorItem.getLinkFloorPlan()));
+
+    }
+
+    public void openWebURI(Uri uri) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(browserIntent);
+    }
+
+
 }
