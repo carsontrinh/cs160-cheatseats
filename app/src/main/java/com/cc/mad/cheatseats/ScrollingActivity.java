df49bdd8672c95aeb3456e0d45f9fe34eec8db86
Daniel Chung <dan.chung.526@berkeley.edu>
2018-12-04 00:35:50 -0800

    Fixed keeping it.

--- app/src/main/java/com/cc/mad/cheatseats/ScrollingActivity.java
+++ app/src/main/java/com/cc/mad/cheatseats/ScrollingActivity.java
@@ -534,6 +534,7 @@ public class ScrollingActivity extends BaseActivity {
             SpaceCardItem filteredSpace = new SpaceCardItem(space.getSpaceName(), space.getSpaceType());
             ArrayList<FloorItem> filteredFloors = new ArrayList<>();
             filteredSpace.setFloors(filteredFloors);
+            filteredSpace.setCrowdedness(space.getCrowdedness());
             for (FloorItem floor : space.getFloors()) {
                 if (passesFilter(floor)) {
                     filteredFloors.add(floor);
